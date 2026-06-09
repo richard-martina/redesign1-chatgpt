31fec9db778c60bd148038e1e3001776acb5078c
richard-martina <richard.martina@gmail.com>
2026-06-09 18:02:59 +0200

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,15 @@
+import Navbar from "@/components/navbar";
+import Hero from "@/components/hero";
+import Metrics from "@/components/metrics";
+import Services from "@/components/services";
+
+export default function HomePage() {
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <Metrics />
+      <Services />
+    </>
+  );
+}
